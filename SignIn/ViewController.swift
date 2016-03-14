84352d83849942bac3be041ddcd22449c363bc65
VipinAgarwal <vipinagarwal@live.com>
2016-03-13 23:12:22 -0700

    Initial Commit

---
+++ SignIn/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SignIn
+//
+//  Created by Vipin Agarwal on 3/13/16.
+//  Copyright © 2016 Vipin Agarwal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
